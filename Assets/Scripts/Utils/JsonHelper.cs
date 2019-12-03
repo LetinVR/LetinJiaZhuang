55eeed74b807da4426ab6ce1e66dfaea2cf9ca98
DESKTOP-NJBH37K\myth <750687181@qq.com>
2019-12-03 18:19:05 +0800

    更改ui交互

---
+++ Assets/Scripts/Utils/JsonHelper.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+
+namespace Extend
+{
+    public static class JsonHelper
+    {
+        public static T Deserialize<T>(this string str)
+        {
+            return JsonConvert.DeserializeObject<T>(str);
+        }
+        public static string Serialize(this object obj)
+        {
+            return JsonConvert.SerializeObject(obj);
+        }
+    }
+}
+
